79f2f33664c14da190ccdc21d7a49a3f7aadd9e0
Akhilesh <akhileshrajakdeveloper@gmail.com>
2024-04-08 14:40:54 +0530

    new feature added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter
+import Leave from './component/leave/Leave';
 import Navbar from './component/navbar/Navbar';
+import Sidebar from './component/navbar/Sidebar';
 import Profile from './component/profile/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Profile/>
+      <div className="AppGlass">
+        <Router> {/* Wrap your component hierarchy with Router */}
+          <Navbar />
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={""} />
+            <Route path="/profile" element={<Profile/>} />
+            <Route path="/me" element={<Leave/>} />
+            {/* Define other routes here */}
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ src/component/profile/ProfileDetail.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const ProfileDetail = () => {
+  return (
+    <>
+    <div className='border-2 md:mt-10 m-6'>
+        <h1 className='border-b-2 p-2 m-6 text-sm md:text-lg'>PRIMARY DETAILS</h1>
+        <div className='md:flex p-2 m-6 mb-10 md:mb-20 mt-6 md:mt-10 text-sm md:text-lg font-light'>
+            <p className='mr-20'>FIRST NAME</p>
+            <p className='mr-20'>MIDDLE NAME</p>
+            <p className='mr-20'>LAST NAME</p>
+            <p className='mr-20'>DISPLAY NAME</p>
+            <p className='mr-20'>GENDER</p>
+        </div>
+        <div className='md:flex p-2 m-6 md:mb-20 text-sm md:text-lg font-light'>
+            <p className='mr-20'>DATE OF BIRTH</p>
+            <p className='mr-20'>MARITAIL STATUS</p>
+            <p className='mr-20'>BLOOD GROUP</p>
+            <p className='mr-20'>PHYSICALLY HANDICAPPED</p>
+            <p className='mr-20'>NATIONALITY</p>
+        </div>
+    </div>
+
+    <div className='border-2 md:mt-10 m-6'>
+        <h1 className='border-b-2 p-2 m-6 text-sm md:text-lg'>CONTACT DETAILS</h1>
+        <div className='md:flex p-2 m-6 mb-10 md:mb-20 mt-6 md:mt-10 text-sm md:text-lg font-light'>
+            <p className='mr-20'>WORK EMAIL</p>
+            <p className='mr-20'>PERSONAL EMAIL</p>
+            <p className='mr-20'>MOBILE PHONE</p>
+            <p className='mr-20'>EMERGRNCY CONTACT 1 NAME</p>
+            <p className='mr-20'>GENDER</p>
+        </div>
+        <div className='md:flex p-2 m-6 md:mb-20 text-sm md:text-lg font-light'>
+            <p className='mr-20'>DATE OF BIRTH</p>
+            <p className='mr-20'>MARITAIL STATUS</p>
+            <p className='mr-20'>BLOOD GROUP</p>
+            <p className='mr-20'>PHYSICALLY HANDICAPPED</p>
+            <p className='mr-20'>NATIONALITY</p>
+        </div>
+    </div>
+
+    <div className='border-2 md:mt-10 m-6'>
+        <h1 className='border-b-2 p-2 m-6 text-sm md:text-lg'>EDUCATION</h1>
+        <div className='md:flex p-2 m-6 mb-10 md:mb-20 mt-6 md:mt-10 text-sm md:text-lg font-light'>
+            <p className='mr-20'>BRANCH/SPECAILIZATION</p>
+            <p className='mr-20'>CGPA/PRECENTAGE</p>
+            <p className='mr-20'>DEGREE</p>
+            <p className='mr-20'>UNIVERSITY/COLLEGE</p>
+            <p className='mr-20'>YEAR OF COMPLETION</p>
+            <p className='mr-20'>YEAR OF JOINING</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ProfileDetail
